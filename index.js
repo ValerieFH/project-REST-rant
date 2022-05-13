a967804900aa93941aabd3efcef24ec6025800eb
Valerie Haupt <valre@valeries-mbp-2.attlocal.net>
2022-05-13 15:50:44 -0500

    added 404 stuff

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+
+app.get('/', (req, res) => {
+    res.send('Howdy yall')
+})
+
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>Dang, 404, yall</h1>')
+})
+
+app.listen(process.env.PORT)
