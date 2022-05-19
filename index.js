94d93aeb749b6b2a04b8d766540dc5e8bf5d2ccc
Valerie Haupt <valre@Valeries-MacBook-Pro-2.local>
2022-05-19 16:53:56 -0500

    added new place view

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
+const PORT = process.env.PORT
 
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
@@ -19,6 +20,6 @@ app.get('*', (req, res) => {
     res.render('error404')
 })
 
-app.listen(process.env.PORT)
+app.listen(PORT, () => console.log(`Live and worldwide at port ${PORT}`))
 
 // Colors to use, Deep Taupe (#815E5B), Purple (#7A6F9B), Light Purple (#D4CDF4)
