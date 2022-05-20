9eaa6c497fa17f66146dc13dc9558eeb30ec23e3
Valerie Haupt <valre@Valeries-MacBook-Pro-2.local>
2022-05-19 19:29:45 -0500

    show page routed

--- controllers/places.js
+++ controllers/places.js
@@ -9,8 +9,18 @@ router.get('/new', (req,res) => {
     res.render('places/new')
 })
 
+router.get('/:id', (req,res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    } else if (!places[id]){
+        res.render('error404')
+    } else {
+        res.render('places/show', {  places: places[id]  })
+    }
+})
+
 router.post('/', (req, res) => {
-    console.log(req.body)
     if (!req.body.pic) {
         req.body.pic = 'http://placekitten.com/400/400'
     }
