301cb754686c95202713945760f11012494f6511
Valerie Haupt <valre@Valeries-MacBook-Pro-2.local>
2022-05-26 13:26:51 -0500

    fixed show page to use new model

--- models/places.js
+++ models/places.js
@@ -1,17 +1,3 @@
-// module.exports = [{
-//     name: 'H-Thai-ML',
-//     city: 'Seattle',
-//     state: 'WA',
-//     cuisines: 'Thai, Pan-Asian',
-//     pic: '/images/thai-food.jpeg'
-//   }, {
-//     name: 'Coding Cat Cafe',
-//     city: 'Phoenix',
-//     state: 'AZ',
-//     cuisines: 'Coffee, Bakery',
-//     pic: '/images/cat-coffee.jpeg'
-//   }]
-
 const mongoose = require('mongoose')
 
 const placeSchema = new mongoose.Schema({
