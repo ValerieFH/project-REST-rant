a029f14f485bfb50ddd4e1ee0fc36cb7df659bb1
Valerie Haupt <valre@Valeries-MacBook-Pro-2.local>
2022-05-27 10:17:14 -0500

    added seed script

--- controllers/places.js
+++ controllers/places.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
 const db = require('../models')
 
+//READ ROUTES
+//Read Index
 router.get('/', (req,res) => {
     db.Place.find()
     .then((places) => {
@@ -12,10 +14,7 @@ router.get('/', (req,res) => {
     })
 })
 
-router.get('/new', (req,res) => {
-    res.render('places/new')
-})
-
+//Read one by id
 router.get('/:id', (req,res) => {
     db.Place.findById(req.params.id)
     .then(place => {
@@ -27,27 +26,39 @@ router.get('/:id', (req,res) => {
     })
 })
 
+//CREATE ROUTES
+//Create one GET
+router.get('/new', (req,res) => {
+    res.render('places/new')
+})
+
+//Create one POST
+router.post('/', (req, res) => {
+    db.Place.create(req.body)
+    .then(() => {
+        res.redirect('/places')
+    })
+    .catch(err => {
+        console.log(err)
+        res.render('error404')
+    })
+})
+
+//UPDATE ROUTES
+//Update GET route
 router.get('/:id/edit', (req, res) => {
     res.send('Get edit form stub')
 })
 
-router.post('/', (req, res) => {
-   db.Place.create(req.body)
-   .then(() => {
-       res.redirect('/places')
-   })
-   .catch(err => {
-    console.log(err)
-    res.render('error404')
-    })
-})
-
-router.delete('/:id', (req, res) => {
-    res.send('DELETE /places/:id stub')
-})
-
+//Update PUT route
 router.put('/:id', (req, res) => {
     res.send('PUT /places/:id stub')
+})
+
+//DELETE ROUTES
+//Delete one
+router.delete('/:id', (req, res) => {
+    res.send('DELETE /places/:id stub')
 })
 
 router.post('/:id/rant', (req, res) => {
--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 
 // Database connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true})
