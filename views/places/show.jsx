b6c270355d79f48523eae66ec9b5781d4d3fdc7a
Valerie Haupt <valre@Valeries-MacBook-Pro-2.local>
2022-06-03 20:22:07 -0500

    Added average rating to show page

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -8,7 +8,21 @@ function show(data){
             No comments yet!
         </h3>
     )
+    let rating = (
+        <h3 className="inactive">
+            Not yet rated
+        </h3>
+    )
     if (data.place.comments.length) {
+        let sumRatings = data.place.comments.reduce((tot, c) => {
+            return tot + c.stars
+        }, 0)
+        let averageRating = sumRatings / data.place.comments.length
+        rating = (
+            <h3>
+            {Math.round(averageRating)} stars
+            </h3>
+        )
         comments = data.place.comments.map(c => {
             return (
                 <div className="border">
@@ -26,6 +40,9 @@ function show(data){
         <Def>
             <main>
                 <h1>{ data.place.name }</h1>
+                <h2>Rating</h2>
+                {rating}
+                <br />
                 <img src={data.place.pic} alt={data.place.name}></img>
                 <h2>Rating</h2>
                 <p>Not Rated</p>
